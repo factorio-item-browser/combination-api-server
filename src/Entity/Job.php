716b6b93b45695bd32b5db1cbc5487350e3eab16
BluePsyduck <bluepsyduck@gmx.com>
2021-12-09 21:13:32 +0100

    Changed Doctrine to use Attributes instead of XML configuration.

--- src/Entity/Job.php
+++ src/Entity/Job.php
@@ -8,6 +8,21 @@ use DateTimeImmutable;
 use DateTimeInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\CustomIdGenerator;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\OneToMany;
+use Doctrine\ORM\Mapping\OrderBy;
+use Doctrine\ORM\Mapping\Table;
+use FactorioItemBrowser\CombinationApi\Server\Doctrine\Type\JobPriorityType;
+use FactorioItemBrowser\CombinationApi\Server\Doctrine\Type\JobStatusType;
+use Ramsey\Uuid\Doctrine\UuidBinaryType;
+use Ramsey\Uuid\Doctrine\UuidGenerator;
 use Ramsey\Uuid\UuidInterface;
 
 /**
@@ -16,15 +31,39 @@ use Ramsey\Uuid\UuidInterface;
  * @author BluePsyduck <bluepsyduck@gmx.com>
  * @license http://opensource.org/licenses/GPL-3.0 GPL v3
  */
+#[Entity]
+#[Table(options: [
+    'charset' => 'utf8mb4',
+    'collate' => 'utf8mb4_bin',
+    'comment' => 'The table holding the export jobs.',
+])]
 class Job
 {
+    #[Id]
+    #[Column(type: UuidBinaryType::NAME, options: ['comment' => 'The id of the job.'])]
+    #[GeneratedValue(strategy: 'CUSTOM')]
+    #[CustomIdGenerator(class: UuidGenerator::class)]
     private UuidInterface $id;
+
+    #[ManyToOne(targetEntity: Combination::class, inversedBy: 'jobs')]
+    #[JoinColumn(name: 'combinationId')]
     private Combination $combination;
+
+    #[Column(type: JobPriorityType::NAME, options: ['comment' => 'The priority of the export job.'])]
     private string $priority;
+
+    #[Column(type: JobStatusType::NAME, options: ['comment' => 'The current status of the export job.'])]
     private string $status;
+
+    #[Column(type: Types::DATETIME_MUTABLE, options: ['comment' => 'The creation time of the job.'])]
     private DateTimeInterface $creationTime;
+
+    #[Column(type: Types::TEXT, options: ['comment' => 'The error message in case the job failed.'])]
     private string $errorMessage;
+
     /** @var Collection<int, JobChange> */
+    #[OneToMany(mappedBy: 'job', targetEntity: JobChange::class)]
+    #[OrderBy(['timestamp' => 'ASC'])]
     private Collection $changes;
 
     public function __construct()
--- src/Entity/JobChange.php
+++ src/Entity/JobChange.php
@@ -5,6 +5,18 @@ declare(strict_types=1);
 namespace FactorioItemBrowser\CombinationApi\Server\Entity;
 
 use DateTimeInterface;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\CustomIdGenerator;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\JoinColumn;
+use Doctrine\ORM\Mapping\ManyToOne;
+use Doctrine\ORM\Mapping\Table;
+use FactorioItemBrowser\CombinationApi\Server\Doctrine\Type\JobStatusType;
+use Ramsey\Uuid\Doctrine\UuidBinaryType;
+use Ramsey\Uuid\Doctrine\UuidGenerator;
 use Ramsey\Uuid\UuidInterface;
 
 /**
@@ -13,12 +25,31 @@ use Ramsey\Uuid\UuidInterface;
  * @author BluePsyduck <bluepsyduck@gmx.com>
  * @license http://opensource.org/licenses/GPL-3.0 GPL v3
  */
+#[Entity]
+#[Table(options: [
+    'charset' => 'utf8mb4',
+    'collate' => 'utf8mb4_bin',
+    'comment' => 'The table holding the changes of the export jobs.',
+])]
 class JobChange
 {
+    #[Id]
+    #[Column(type: UuidBinaryType::NAME, options: ['comment' => 'The internal id of the job change.'])]
+    #[GeneratedValue(strategy: 'CUSTOM')]
+    #[CustomIdGenerator(class: UuidGenerator::class)]
     private UuidInterface $id;
+
+    #[ManyToOne(targetEntity: Job::class, inversedBy: 'changes')]
+    #[JoinColumn(name: 'jobId')]
     private Job $job;
+
+    #[Column(length: 255, options: ['comment' => 'The initiator of the change.'])]
     private string $initiator;
+
+    #[Column(type: JobStatusType::NAME, options: ['comment' => 'The new status of the export job.'])]
     private string $status;
+
+    #[Column(type: Types::DATETIME_MUTABLE, options: ['comment' => 'The time of the change.'])]
     private DateTimeInterface $timestamp;
 
     public function setId(UuidInterface $id): self
